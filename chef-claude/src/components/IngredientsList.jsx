444ff6d3581ede6aa11117a0f44791fade922ca6
Adedamola <aderogbaadedamola8@gmail.com>
2025-10-06 01:22:20 +0100

    add delete icon

--- chef-claude/src/components/IngredientsList.jsx
+++ chef-claude/src/components/IngredientsList.jsx
@@ -2,15 +2,13 @@
 
 function IngredientsList(props) {
 
-    const ingredientsListItems = props.ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
+    
     return(
         <section>
                     <h2>Ingredients on hand:</h2>
-                    <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                    <ul className="ingredients-list" aria-live="polite">{props.ingredientsListItems}</ul>
                     {props.ingredients.length > 3 && <div className="get-recipe-container">
-                            <div>
+                        <div>
                                 <h3>Ready for a recipe?</h3>
                                 <p>Generate a recipe from your list of ingredients.</p>
                             </div>
--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -2,24 +2,38 @@ import React from "react"
 import IngredientsList from "./IngredientsList"
 import ClaudeRecipe from "./ClaudeRecipe"
 import { getRecipeFromMistral } from "../ai"
+import deleteIcon from "../assets/deleteIcon.svg"
 import Loader from "./Loader"
 
 function Main() {
 
 
     const [ingredients, setIngredients] = React.useState([])
+     const [recipe, setRecipe] = React.useState("")
+
+     const ingredientsListItems = ingredients.map((ingredient, index) => (
+        <li key={ingredient} className="ingredients">
+            {ingredient} 
+            <span 
+                onClick={() => deleteIngredient(index)}
+                className="delete-container">
+                <img src={deleteIcon} alt="delete icon" />
+            </span>
+        </li>
+      ))
 
     function submitAction(formData) {
         const newIngredient = formData.get("ingredient")
-        console.log(newIngredient);
         setIngredients((prevIngredients)=> [...prevIngredients, newIngredient])
     }
-    const [recipe, setRecipe] = React.useState("")
-
+   
     async function getRecipe(){
         
         let receivedRecipe = await getRecipeFromMistral(ingredients)
         setRecipe(receivedRecipe)
+    }
+    function deleteIngredient(indexToDelete){
+            setIngredients(prev => prev.filter((_, index)=> index !== indexToDelete ))
     }
     
     
@@ -40,6 +54,8 @@ function Main() {
                 <IngredientsList 
                     ingredients={ingredients}
                     getRecipe={getRecipe}
+                    ingredientsListItems={ingredientsListItems}
+                    deleteIngredient={deleteIngredient}
                 />}
             {recipe &&  <ClaudeRecipe recipe={recipe}/>}
         </main>
